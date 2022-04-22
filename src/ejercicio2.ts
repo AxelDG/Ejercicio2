a311480feed937106bc7312ba096fc47f85352d8
AxelDG <104107385+AxelDG@users.noreply.github.com>
2022-04-22 20:14:45 -0300

    Initial commit

---
+++ src/ejercicio2.ts
@@ -0,0 +1,28 @@
+
+//Ejercicio 1
+console.log("Mientras haya un camino adelante");
+console.log("Avanzar");
+console.log("Eliminar 1");
+console.log("Gira a la izquierda");
+console.log("Mientras haya un camino adelante");
+console.log("Avanzar");
+console.log("Eliminar 1");
+
+//Ejercicio 2
+console.log("Mientras haya camino adelante");
+console.log("Avanzar");
+console.log("Si hay un agujero");
+console.log("Llenar 1");
+console.log("Si hay una pila");
+console.log("Quitar 1");
+
+
+//Ejercicio 3
+console.log("Gira a la izquierda");
+console.log("Avanza");
+console.log("Gira a la derecha");
+console.log("Avanzar");
+console.log("Avanzar");
+console.log("Girar a la derecha");
+console.log("Avanzar");
+console.log("Eliminar 1");
